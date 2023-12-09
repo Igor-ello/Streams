758fe94b30d17d9cbf772e553a033448e659879f
Igorello <ioproglab@gmail.com>
2023-12-09 13:08:09 +0300

    Chaos in threads

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
 public class Main {
+    private static int MAX_VALUE = 20;
+
     public static void main(String[] args) {
-        
+        Thread evenThread = new Thread(new Runnable() { //чётные
+            @Override
+            public void run() {
+                for (int i = 2; i < MAX_VALUE; i++){
+                    System.out.println("Первый поток " + i);
+                    try {
+                        Thread.sleep(200);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+        Thread oddThread = new Thread(new Runnable() { //не чётные
+            @Override
+            public void run() {
+                for (int i = 1; i < MAX_VALUE; i++){
+                    System.out.println("Второй поток " + i);
+                    try {
+                        Thread.sleep(200);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+
+        evenThread.start(); //через run() поток в главном потоке
+        oddThread.start();
     }
 }
