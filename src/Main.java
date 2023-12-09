79014c80c80a66610f7697834428411208ebc59c
Igorello <ioproglab@gmail.com>
2023-12-09 13:21:33 +0300

    Work stream 1-2-1-2-1-2 ...

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     private static int MAX_VALUE = 20;
     static final Object lock = new Object();
+    static Boolean isEvenTrue = true;
 
     public static void main(String[] args) {
         Thread evenThread = new Thread(new Runnable() { //чётные
@@ -8,7 +9,16 @@ public class Main {
             public void run() {
                 for (int i = 2; i < MAX_VALUE; i++){
                     synchronized (lock) { // не заходим в это поле, плка выполняется другой поток
+                        while (!isEvenTrue) {
+                            try {
+                                lock.wait();
+                            } catch (InterruptedException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
                         System.out.println("Первый поток " + i);
+                        isEvenTrue = false;
+                        lock.notify();
                     }
                 }
             }
@@ -18,7 +28,16 @@ public class Main {
             public void run() {
                 for (int i = 1; i < MAX_VALUE; i++){
                     synchronized (lock) {
+                        while (isEvenTrue) {
+                            try {
+                                lock.wait();
+                            } catch (InterruptedException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
                         System.out.println("Второй поток " + i);
+                        isEvenTrue = true;
+                        lock.notify();
                     }
                 }
             }
